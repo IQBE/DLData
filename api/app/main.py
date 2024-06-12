ca41900b480ea818840047c931d62d1cf1447c3d
Ilya Quateau <git@quateau.net>
2024-06-12 08:42:04 +0000

    Removed todo to start from a blank slate

--- api/app/main.py
+++ api/app/main.py
@@ -1,11 +1,6 @@
 from fastapi import FastAPI
-from sqlalchemy import text
-
-from app.todos import router as todos_router
 
 app = FastAPI(root_path="/api")
-
-app.include_router(todos_router, prefix="/todos", tags=["todos"])
 
 @app.get("/")
 async def read_root():
