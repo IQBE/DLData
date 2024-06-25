7cdf979c5bb9e21f90d220c2a14db4990aa1d973
Ilya Quateau <git@quateau.net>
2024-06-25 14:30:21 +0000

    dbt setup for vehicle_updates

--- etl/dldata_dbt/models/vehicle_updates/avg_delay.sql
+++ etl/dldata_dbt/models/vehicle_updates/avg_delay.sql
@@ -2,6 +2,6 @@ SELECT
     date_trunc ('day', "timestamp") AS DATE,
     AVG(departure_delay) AS avg_delay
 FROM
-    vehicle_updates
+    {{ source('public', 'vehicle_updates') }}
 GROUP BY
     date_trunc ('day', "timestamp")
